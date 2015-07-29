8f3744203d6b43ec64b20576c9320a09b799afe3
Jon Chen <bsd@voltaire.sh>
2015-07-29 02:21:25 -0700

    0.1.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='docku.build',
-    version='0.0.1',
+    version='0.1.0',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['docku'],
     include_package_data=True,
